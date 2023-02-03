bf3a3b45e32242ec0c571a33ebc7608ded6b517d
Zach Boudreaux <zach.boudreaux999@gmail.com>
2023-02-03 09:01:47 -0500

    updated python web server

--- script.sh
+++ script.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 FILE=index.html
-[ -f $FILE ] && rm -rf $FILE
+
 # if test -f $FILE
 # then
 #     echo "EXISTS"
 #     rm -rf $FILE
 # fi
+[ -f $FILE ] && rm -rf $FILE #shorthand
 
-curl https://bitbucket.org/bjgiller/evt-tech-challenge/raw/master/evt-web.html >> $FILE
-python3 server.py
+curl https://bitbucket.org/bjgiller/evt-tech-challenge/raw/master/evt-web.html >> $FILE #gets html from provided URL
+python3 server.py #runs server.py and starts server
--- server.py
+++ server.py
@@ -1,15 +1,34 @@
 import http.server
 import socketserver
 
+HOST = "localhost"
 PORT = 8080
 
-class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.path = "index.html"
-        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+# class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+#     def do_GET(self):
+#         self.path = "index.html"
+#         return http.server.SimpleHTTPRequestHandler.do_GET(self)
     
-Handler = MyHttpRequestHandler
+# Handler = MyHttpRequestHandler
 
-with socketserver.TCPServer(("", PORT) , Handler) as httpd:
-    print("Web Server running at port: " + str(PORT))
-    httpd.serve_forever()
+# with socketserver.TCPServer(("", PORT) , Handler) as httpd:
+#     print("Web Server running at port: " + str(PORT))
+#     httpd.serve_forever()
+
+class MyServer(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.path = "index.html"
+
+if __name__ == "__main__":
+    myWebServer = http.server.HTTPServer((HOST, PORT), MyServer)
+    print("Web Server started on port: %s" % str(PORT))
+    print("Access it at http://%s:%s" % (HOST, PORT))
+    
+    try:
+        myWebServer.serve_forever()
+    except KeyboardInterrupt:
+        myWebServer.server_close()
+        print("Web server has been stopped.")
+    except:
+        print("Something went wrong.")
